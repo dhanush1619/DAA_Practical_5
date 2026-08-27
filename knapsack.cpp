d5d94541923d4ba65a0ce298cdb306bea183e7ba
dhanush1619 <kd331819@gmail.com>
2026-08-27 17:17:42 +0530

    knapsack.cpp

--- knapsack.cpp
+++ knapsack.cpp
@@ -1,40 +1,55 @@
-#include <bits/stdc++.h>
+#include <iostream>
+#include <vector>
+#include <algorithm>
 using namespace std;
 
-int knapsack(int W, vector<int>& wt, vector<int>& val, int n) {
-    vector<vector<int>> dp(n + 1, vector<int>(W + 1, 0));
+int main() {
+    int n, capacity;
 
+    // Number of items
+    cout << "Enter number of items: ";
+    cin >> n;
+
+    // Knapsack capacity
+    cout << "Enter knapsack capacity: ";
+    cin >> capacity;
+
+    vector<int> weight(n);
+    vector<int> value(n);
+
+    // Input weights
+    cout << "Enter weights of " << n << " items: ";
+    for (int i = 0; i < n; i++) {
+        cin >> weight[i];
+    }
+
+    // Input values
+    cout << "Enter values of " << n << " items: ";
+    for (int i = 0; i < n; i++) {
+        cin >> value[i];
+    }
+
+    // DP table
+    vector<vector<int>> dp(n + 1, vector<int>(capacity + 1, 0));
+
+    // 0/1 Knapsack
     for (int i = 1; i <= n; i++) {
-        for (int w = 0; w <= W; w++) {
-            // Don't take the current item
+        for (int w = 1; w <= capacity; w++) {
+
+            // Don't take the item
             dp[i][w] = dp[i - 1][w];
 
-            // Take the current item if it fits
-            if (wt[i - 1] <= w) {
+            // Take the item if it fits
+            if (weight[i - 1] <= w) {
                 dp[i][w] = max(
                     dp[i][w],
-                    val[i - 1] + dp[i - 1][w - wt[i - 1]]
+                    value[i - 1] + dp[i - 1][w - weight[i - 1]]
                 );
             }
         }
     }
 
-    return dp[n][W];
-}
-
-int main() {
-    int n, W;
-    cin >> n >> W;
-
-    vector<int> wt(n), val(n);
-
-    for (int i = 0; i < n; i++)
-        cin >> wt[i];
-
-    for (int i = 0; i < n; i++)
-        cin >> val[i];
-
-    cout << knapsack(W, wt, val, n) << endl;
+    cout << "Maximum value = " << dp[n][capacity] << endl;
 
     return 0;
 }
